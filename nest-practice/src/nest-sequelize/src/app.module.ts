6de4e49250638714c1555b9c0ad2928e4c569a17
darshan-devani-777 <darshan.devani@tecocraft.com>
2025-11-20 18:41:58 +0530

    Nest.Js

--- nest-practice/src/nest-sequelize/src/app.module.ts
+++ nest-practice/src/nest-sequelize/src/app.module.ts
@@ -3,17 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot({
-      dialect: 'mysql',
-      host: 'localhost',
-      username: 'root',
-      password: '',
-      database: 'nest_sequelize',
-      autoLoadModels: true,
-      synchronize: false,
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    SequelizeModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        dialect: 'mysql',
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT') || '3306', 10),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        autoLoadModels: true,
+        synchronize: false,
+      }),
+      inject: [ConfigService],
     }),
     UsersModule,
   ],
