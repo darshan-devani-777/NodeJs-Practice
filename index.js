33b51a6bb6080fd5b9d8df71da2838e9c6bd5d15
darshan-devani-777 <darshan.devani@tecocraft.com>
2025-11-21 11:43:47 +0530

    Nest.Js

--- index.js
+++ index.js
@@ -634,6 +634,23 @@
                 // HEADER	             Version in custom header	       X-API-Version: 2
                 // MEDIA_TYPE	             Version in Accept header	    Accept: application/json;v=2
                 // CUSTOM	     You define how version is extracted      ?version=2, cookie, etc.
+
+                // <!-- v1 Users -->
+                // curl -s http://localhost:3000/v1/users | jq
+
+                // <!-- v2 Users -->
+                // curl -s http://localhost:3000/v2/users | jq
+
+                // <!-- Neutral Routes -->
+                // GET → /v1/status
+                // curl -s http://localhost:3000/v1/status | jq
+
+                // GET → /v2/status
+                // curl -s http://localhost:3000/v2/status | jq
+
+                // GET → /status
+                // curl -s http://localhost:3000/status | jq
+
  
 // Events - message or signal emitted with handled asynchronously
            //  - Emitter: A part of your code that emits an event (e.g., when a user signs up).
